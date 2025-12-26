b3eb697a0d0ce4f6ab4b14aff13871dc92a2b5ae
Moldo <moldovanalex308@yahoo.com>
2025-12-26 18:52:42 +0200

    Initial Commit

---
+++ Client/messages.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    chat=document.getElementById("chat");
+    const buton = document.getElementById("SendToServer");
+    buton.addEventListener("click", () => { sendData() });
+
+    function sendData() {
+        const text=document.getElementById("mesaj_client").value;
+        const data = {
+            message: text
+        };
+
+        const bubble = document.createElement("div");
+            bubble.id="client-messages"
+            bubble.className = "message-bubble-right";
+            bubble.textContent = text;
+            chat.appendChild(bubble);
+
+        fetch("http://localhost:5123/message", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        return;
+    }
+
+  function fetchMessages() {
+    fetch(`http://localhost:8080/get_messages`)
+        .then(res => res.json())
+        .then(data => { 
+
+            const bubble = document.createElement("div");
+            bubble.id="server-messages"
+            bubble.className = "message-bubble-left";
+            bubble.textContent = data.at(-1);
+            chat.appendChild(bubble);
+            
+            fetchMessages();
+        });
+    return;
+}
+    fetchMessages()
+});
---
+++ Server/server.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const EventEmitter = require('events');
+const messageEvents = new EventEmitter();
+let messages_list= [];
+
+const server = http.createServer((req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080"); 
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  if (req.method === "GET" && req.url === "/messages.js") {
+    const filePath = path.join(__dirname, "messages.js");
+    const js = fs.readFileSync(filePath, "utf8");
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    res.end(js);
+  }
+
+  if (req.method === "GET" && req.url === "/chat-bubble.css") {
+    const filePath = path.join(__dirname, "chat-bubble.css");
+    const cs = fs.readFileSync(filePath, "utf8");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(cs);
+  }
+
+  if (req.method === 'GET' && req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    const html = fs.readFileSync(filePath, 'utf8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+  }
+
+  else if (req.method === 'GET' && req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ mesaj: 'Salut - Test API' }));
+  }
+  
+  else if (req.method === 'POST' && req.url === '/message'){
+    let body = "";
+
+    req.on("data", chunk => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const received = JSON.parse(body);
+      messages_list.push(received.message);
+      messageEvents.emit('{Server}Data Modified');
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({
+        status: "ok",
+        message: `Am primit mesajul de la: "${received.message}"`}));
+    })
+  }
+  else if (req.method === 'GET' && req.url === '/get_messages') {
+    const trimiteDate = () => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(messages_list));
+      };
+    messageEvents.once('{Server}Data Modified', trimiteDate);
+    req.on('close', () => {
+            messageEvents.removeListener('{Server}Data Modified', trimiteDate);
+        });
+  };
+});
+
+server.listen(5123, "0.0.0.0", () => {
+  console.log('Serverul rulează pe http://localhost:5123');
+});
